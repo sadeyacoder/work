1a00549a3699a11f932aca9316b886cd29b8776d
sadeyacoder <sadeyapampori@gmail.com>
2026-07-13 23:55:22 +0530

    1

---
+++ next.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        System.out.println("My  unique pazzta Recipe");
+        pazz();
+    }
+
+    public static void pazz(){
+        System.out.println("1.Boil pazzta in pan");
+        System.out.println("2.Now add your special pazzta recipe after this");
+    }
+}
