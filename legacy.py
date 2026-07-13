568572002dc66e2ce621b20506010dc74f81cedd
sadeyacoder <sadeyapampori@gmail.com>
2026-07-13 21:51:52 +0530

    2

---
+++ legacy.py
@@ -0,0 +1,14 @@
+import keyword
+print("Hey , welcome to codingal")
+
+#Printinga Number
+print(45)
+
+#Next Line after print
+print("Hello World \n")
+
+#print keywords
+print("Keywords in Python are....")
+
+
+print(keyword.kwlist)
