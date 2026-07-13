64a6c69b1aba7e86a8c527500875afbf5817d876
sadeyacoder <sadeyapampori@gmail.com>
2026-07-13 21:48:09 +0530

    1

---
+++ print comment.py
@@ -0,0 +1,10 @@
+print("Hello world!")
+
+# Printing my name
+print("My name is sadeya.")
+
+#hobby
+print("I love gardening.")
+
+# printing a simple calculation
+print("The result of 3 + 3 is:", 3 + 3)
