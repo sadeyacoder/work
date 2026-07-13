250b2ab24d683b6f27c9421d65784d22e7a4ecde
sadeyacoder <sadeyapampori@gmail.com>
2026-07-13 23:03:21 +0530

    2

--- intro.py
+++ intro.py
@@ -5,3 +5,25 @@ def introduction(name):
 user_name = input("Enter your name")
 introduction(user_name)
 
+
+
+
+
+
+#FACTORIAL OF A NUMBER
+
+def factorial(n):
+   if n == 1:
+       return n
+   else:
+       return n*factorial(n-1)
+
+num = int(input("Enter a number of your choice"))
+
+
+if num < 0:
+   print("You have entered a negative number")
+elif num == 0:
+   print("Factorial of 0 is 1")
+else:
+   print("Factorial of", num, "is", factorial(num))
